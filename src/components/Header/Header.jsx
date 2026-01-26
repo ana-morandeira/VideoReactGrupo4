adf0049ddc149297050905e0cf53cc84fcca66ec
Alberto <bettogp94@gmail.com>
2026-01-26 11:59:36 +0100

    feat: I create a navbar component responsive with Tailwind CSS. Also, i add another component called header for create Header and Banner component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,8 @@
-import image from "./logoVideoclub.png"
+import Navbar from "./Navbar/Navbar";
 
 function Header() {
     return (
-        <header className="bg-blue-600">
-            <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
-                <span className="logoImage inline-flex items-center gap-2">
-                    <img
-                        src={image}
-                        alt="Logo"
-                        className="w-[50px] h-[50px] inline-block"
-                    />
-                    <p className="text-white font-semibold text-lg">
-                        React Project
-                    </p>
-                </span>
-
-                <span className="text-white text-base">
-                    Cards with Props
-                </span>
-
-                <span className="text-white text-sm">
-                    Academic Practice
-                </span>
-            </div>
-        </header>
+        <Navbar />
     )
 }
 
---
+++ src/components/Header/Navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+function Navbar() {
+  const [isOpen, IsOpenCheck] = useState(false);
+
+
+    
+ 
+
+  return (
+    <header className="flex flex-col lg:flex-row p-6 lg:p-8 border-b-2">
+      <div className="flex justify-center items-center w-full lg:w-2/6 mb-4 lg:mb-0">
+        <img
+          src="src/assets/logos/iberianMovieLogo.svg"
+          alt="Logo de la Aplicación Web del Videoclub"
+          className="max-w-[30%] mr-4"
+        />
+        <h1 className="text-4xl lg:text-5xl font-bold">
+          Iberian <span className="text-amber-300">Movie</span>
+        </h1>
+      </div>
+
+        <div>
+            <button className="lg:hidden" onClick={()=>IsOpenCheck(!isOpen)}>{isOpen ? "✖" : "☰"} </button>
+        </div>
+      
+      <nav className={`w-full lg:w-2/6 ${isOpen ? "flex" : "hidden"} md:flex` }>
+        <ul className="flex flex-col lg:flex-row items-center justify-center w-full text-center text-2xl lg:text-3xl">
+          <li className="flex-1 flex justify-center my-4 border-b-2 w-full lg:border-none">
+            <a href="" className="hover:text-amber-200">
+              Explore
+            </a>
+          </li>
+          <li className="flex-1 flex justify-center my-4 border-b-2 w-full lg:border-none">
+            <a href="" className="hover:text-amber-200">
+              Directors
+            </a>
+          </li>
+          <li className="flex-1 flex justify-center my-4 border-b-2 w-full lg:border-none">
+            <a href="" className="hover:text-amber-200">
+              My List
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="flex justify-center items-center w-full lg:w-2/6 mt-4 lg:mt-0">
+        <input
+          type="text"
+          className="bg-white w-4/6 text-black p-4 text-xl lg:w-2/3 lg:text-2xl border-2"
+          placeholder="Buscar Película"
+        />
+      </div>
+    </header>
+  );
+}
+export default Navbar;
