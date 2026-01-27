c7f3be72cfef5ec58d2244b457488a60f9fea8bf
andreselianor <113841548+andreselianor@users.noreply.github.com>
2026-01-27 12:44:43 +0100

    fix: deleted image

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,4 @@
 import Card from "../Card/Card.jsx"
-import image from "../../assets/images/nueve_reinas.jpg"
 import ClassicJewels from "../ClassicJewels/ClassicJewels.jsx";
 import ContemporaryCinema from "../ContemporaryCinema/ContemporaryCinema.jsx";
 function Main() {
